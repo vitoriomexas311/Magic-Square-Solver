b9ac2e042e36eca08fe7ffacdde9e496a393cff9
vitoriomexas311 <139288274+vitoriomexas311@users.noreply.github.com>
2024-05-15 12:54:15 -0400

    All done

--- OddN.java
+++ OddN.java
@@ -1,7 +1,8 @@
 public class OddN {
 
-  public static void main(String[] args) throws RuntimeException {
+  public static void main(String[] args) {
     int n = Integer.parseInt(args[0]);
+
     if (n % 2 == 0) {
       throw new RuntimeException("n must be an odd integer");
     }
@@ -16,17 +17,28 @@ public class OddN {
 
     // Siamese Method
 
-
     magic[row][col] = 1; // start with 1 in the middle of the first row
     for (int i = 2; i < n * n; i++) {
+      // if we reach a corner
       if (magic[(row + 1) % n][(col + 1) % n] == 0) {
         row = (row + 1) % n;
         col = (col + 1) % n;
-
+        // otherwise, we don't change the column, but go through by row
+      } else {
+        row = (row - 1 + n) % n;
       }
-
       magic[row][col] = i;
     }
 
+    // print results
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (magic[i][j] < 10)  System.out.print(" ");  // for alignment
+        if (magic[i][j] < 100) System.out.print(" ");  // for alignment
+        System.out.print(magic[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 }
+
